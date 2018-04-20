e59abc67299119f684b715f5861e74e0e81ff627
theguyfromtv <shaharblonde@The-Tesseract.local>
2018-04-20 19:09:59 -0400

    everything but spotify works

--- liri.js
+++ liri.js
@@ -1,8 +1,9 @@
 require("dotenv").config();
 var fs= require("fs");
 var keys = require("./keys.js");
-var Spotify = require("node-spo");
+var Spotify = require("node-spotify-api");
 var Twitter = require('twitter');
+var request = require("request");
 
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
@@ -27,6 +28,39 @@ switch (action) {
       read();
       break;
     }
+    function read(){
+
+        fs.readFile("random.txt", "utf8", function(error, data) {
+    
+            if (error) {
+              return console.log(error);
+            }
+          
+            console.log(data);
+
+            wordArr=data.split(",")
+
+            wordArr.splice(0,0,"node","liri.js");
+
+            action=wordArr[2]
+            
+            switch (action) {
+                case "my-tweets":
+                  tweets();
+                  break;
+                
+                case "spotify-this-song":
+                  song();
+                  break;
+                
+                case "movie-this":
+                  movie();
+                  break;
+                
+                case "do-what-it-says":
+                  read();
+                  break;
+                }
 
     function tweets(){
         var params = {screen_name: "theguyfromtv",
@@ -52,8 +86,16 @@ switch (action) {
 
         for(var i=3; i<wordArr.length; i++){
 
-            track = track+" "+wordArr[i];
+            if (i > 3 && i < nodeArgs.length) {
 
+                track = track + "+" + wordArr[i];
+            
+              }
+            
+              else {
+            
+                track += wordArr[i];
+            
         }
 
  
@@ -68,3 +110,49 @@ switch (action) {
           
 
     }
+}
+
+  function movie(){
+
+    var movieName = "";
+
+    for (var i = 3; i < wordArr.length; i++) {
+
+        if (i > 3 && i < wordArr.length) {
+
+        movieName = movieName + "+" + wordArr[i];
+
+        }
+
+        else {
+
+        movieName += wordArr[i];
+
+        }
+    }
+
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+
+    request(queryUrl, function(error, response, body) {
+
+        if (!error && response.statusCode === 200) {
+
+            console.log(JSON.parse(body).Title);
+            console.log(JSON.parse(body).Year);
+            console.log(JSON.parse(body).Ratings[0].Source+": ",JSON.parse(body).Ratings[0].Value);
+            console.log(JSON.parse(body).Ratings[1].Source+": ",JSON.parse(body).Ratings[0].Value);
+            console.log(JSON.parse(body).Country);
+            console.log(JSON.parse(body).Language);
+            console.log(JSON.parse(body).Plot);
+            console.log(JSON.parse(body).Actors);
+
+        }
+    });
+
+}
+
+
+
+})
+}
